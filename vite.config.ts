335f150674c5c7f288efc943668325ab4eac0083
christian <xhriscarrasc@gmail.com>
2025-04-14 23:55:57 +0200

    fix: Add viteStaticCopy plugin to copy js-dos assets from node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,14 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/js-dos/dist/*",
+          dest: "js-dos",
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
