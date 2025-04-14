5d4d32ee408fa56ccc21da411e2f66218b93f0c3
christian <xhriscarrasc@gmail.com>
2025-04-15 00:12:41 +0200

    fix: Ensure assetsInclude includes wasm and data files in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       ],
     }),
   ],
+  assetsInclude: ["**/*.wasm", "**/*.data"],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
